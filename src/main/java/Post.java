0b469255300ec564a357798c589cdfd367d77850
DrEvil <opc_senator4@mail.ru>
2022-11-11 00:48:33 +0300

    Почти все сделано

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport;
+    public String patronumic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+
+    public FormDate birthday;
+
+}
